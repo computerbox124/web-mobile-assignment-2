48044ad8c5ea340d3700492ed3babb6712b08172
computerbox <computerbox124@gmail.com>
2023-12-05 23:30:22 +0400

    Documentation and README added

---
+++ README.md
@@ -0,0 +1,5 @@
+Project written in plain html, css and boostrap framework and JS (javascript) and JQuery JS framework.
+It is enough to open index.html and enjoy the Hus Shop :)
+
+Also it is possible to maintain server by using local servers such as apache.
+Also WebCharm provides its own local server, it will open index.html in browser and serve as server.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,9 @@
+// The basic url for fetching API
 const API = 'https://dummyjson.com/products'
 
+// -------- Support functions -------------
+
+// The basic fetching function by URL
 function fetchData(url) {
     console.log(url);
     return $.ajax({
@@ -15,6 +19,7 @@ function fetchData(url) {
 
 }
 
+// The function which gets all available categories
 function getCategories(){
     const APICategories = API + '/categories';
     return fetchData(APICategories).then(function (data){
@@ -22,6 +27,7 @@ function getCategories(){
     });
 }
 
+// The function which gets all products
 function getProducts(link){
     return fetchData(link).then( function(data){
         return {
@@ -34,12 +40,31 @@ function getProducts(link){
 
 
 
-var pageActive = 1;
+// Support function in order to get ID of the HTML object
+function getHTMLId(element) {
+    var res = '';
+    for(var i = 4; i < element.length; i++){
+        res += element[i];
+    }
+    return res
+}
 
+
+
+
+// Function which will open the product details after clicking the product card
+function clickedProduct(id){
+    window.open(`./detailed.html?product=${id}`, '_blank');
+}
+
+// --------------- Component functions --------------------
+
+// Component function which renders pagination buttons
+var pageActive = 1;
 function renderPagination(objCounts, link) {
 
-   getProducts(link).then(function (data){
-       $("#pagination").empty();
+    getProducts(link).then(function (data){
+        $("#pagination").empty();
         const total = data.total;
         var pages = Math.floor(total / objCounts) + (total % objCounts ? 1 : 0);
         for (let i = 1; i <= pages; i++) {
@@ -55,33 +80,25 @@ function renderPagination(objCounts, link) {
             $('<a>', {
                 'class': 'page-link',
             }).text(i).on('click', function () {
-                pagination(i, objCounts, link)
+                pagination(i, objCounts, link + '?')
             }).appendTo('#page' + i);
         }
     });
 }
 
-function clickedProduct(id){
-    window.open(`./detailed.html?product=${id}`, '_blank');
-}
-
-function getHTMLId(element) {
-    var res = '';
-    for(var i = 4; i < element.length; i++){
-        res += element[i];
-    }
-    return res
-}
-
+// Component function which renders the all projects according pagination configuration
 function pagination(page, objectCount, link){
     $("#container").empty();
     var pageId = 'page' + page;
     var pageIdActive = 'page' + pageActive;
 
+    //If other page button clicked, the current button must lose the active class
     $('#' + pageIdActive).removeClass('active');
+    //If other page button clicked, the clicked page button must gain the active class
     $('#' + pageId).addClass('active');
 
     pageActive = page;
+    // Getting products by using pagination configurations
     let skipObjects = (page - 1) * objectCount;
     const paginationAPI = link + 'limit=' + objectCount + '&skip=' + skipObjects
     getProducts(paginationAPI).then(function (data){
@@ -91,6 +108,8 @@ function pagination(page, objectCount, link){
         {
             objects.push(dataEl[i]);
         }
+
+        // Rendering product to HTML
         let cnt = 0;
         let rowCnt = 0;
         for (var i = 0; i < objects.length; i+=3) {
@@ -107,11 +126,13 @@ function pagination(page, objectCount, link){
                 })
                     .on('click', function (){ clickedProduct(getHTMLId(this.id)); })
                     .on('mouseenter',function(){
+                        // When mouse entered the border will appear
                         this.style = '' +
                             'border-color: black;' +
                             'border-style: groove; ' +
                             'border-width: 4px';})
-                    .on('mouseleave',function(){ this.style = '';}).appendTo('#row' + rowCnt);
+                    .on('mouseleave',function(){
+                        this.style = '';}).appendTo('#row' + rowCnt);
 
                 $('<img>', {
                     'src': item.thumbnail,
@@ -153,6 +174,7 @@ function pagination(page, objectCount, link){
 
 }
 
+// Component function which renders all categories
 function renderCategories(){
    $("#SelectFilter").empty();
   getCategories().then(function (data){
@@ -176,20 +198,25 @@ function renderCategories(){
   });
 }
 
+// Component function which renders the whole page
 function generatePage(link, sig){
     renderPagination(12, link);
     pagination(1, 12, link + ( sig ? "?" : '&'));
 
 }
 
+// Render the page when all files loaded
+$(document).ready(function(){
+    renderCategories();
+    generatePage(API, 1);
+});
 
+//------- Binding events -------
+
+// Binding the search event
 $( "#search" ).bind( "change", function(e) {
     var keyword = e.target.value;
     generatePage(API + '/search?q=' + keyword, 0);
 });
 
 
-$(document).ready(function(){
-    renderCategories();
-    generatePage(API, 1);
-});
